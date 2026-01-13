2d8afe103c8db11881c2e625ab0f6c7317304322
yashp387 <yashmp2004@gmail.com>
2026-01-13 14:44:23 +0530

    complete assignment

---
+++ lecture-02/assignment-01.js
@@ -0,0 +1,11 @@
+// Assignment-01 -> Number check
+
+let x = 0;
+
+if(x > 0) {
+    console.log('positive number');
+} else if(x < 0) {
+    console.log('negative number');
+} else {
+    console.log('zero');
+}
