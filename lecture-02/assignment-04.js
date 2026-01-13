f0ace4dfe9f1fd526522fc7aac780b5546fa0331
yashp387 <yashmp2004@gmail.com>
2026-01-13 15:08:27 +0530

    complete assignment-4

---
+++ lecture-02/assignment-04.js
@@ -0,0 +1,11 @@
+// Assignment-04 -> Total Price function
+
+const price = 100;
+const quantity = 5;
+
+const calculateTotalPrice = (p, q) => {
+    let total = p*q;
+    return total;
+}
+
+console.log(calculateTotalPrice(price, quantity));
