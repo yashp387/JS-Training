62d626287ec97a626e435481931d9eb5a420fa18
yashp387 <yashmp2004@gmail.com>
2026-01-13 14:49:26 +0530

    complete assignment-2

---
+++ lecture-02/assignment-02.js
@@ -0,0 +1,11 @@
+// Assignment-01 -> Even/Odd number
+
+for(let i = 1; i <= 20; i++) {
+    console.log(i);
+
+    if(i % 2 == 0) {
+        console.log(`${i} is an even number`);
+    } else {
+        console.log(`${i} is an odd number`);
+    }
+}
