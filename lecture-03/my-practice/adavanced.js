d47ee1a64981dc968e6976dc87f73bef84e7c19d
yashp387 <yashmp2004@gmail.com>
2026-01-19 18:12:31 +0530

    adavanced lvl

---
+++ lecture-03/my-practice/adavanced.js
@@ -0,0 +1,37 @@
+/* Scenario: You are building a feature that converts a flat list of users into a "lookup object" for faster searching.
+
+Input: An array of objects: [{ id: 'a1', name: 'Alice' }, { id: 'b2', name: 'Bob' }]
+
+Task:
+Create a function that uses reduce to turn this array into an object where the id is the key.
+Ensure that the original array objects are not modified.
+Add a method to your result object called updateName(id, newName) that updates a user's name by reference.
+*/
+
+const inputArray = [
+  { id: "a1", name: "Alice" },
+  { id: "b2", name: "Bob" },
+];
+
+const obj = inputArray.reduce((acc, user) => {
+  return {
+    ...acc,
+    [user.id]: {
+      ...user,
+    },
+  };
+}, {});
+
+obj.updateName = function (id, newName) {
+  if (this[id]) {
+    this[id].name = newName;
+    console.log(`Updated ${id} to ${newName}`);
+  } else {
+    console.log("User not found");
+  }
+};
+
+obj.updateName("a1", "yash");
+
+console.log(obj["a1"]);
+console.log(inputArray[0]);
