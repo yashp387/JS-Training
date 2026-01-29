b3372b871f6c71949ec815577cd25f197bb12246
yashp387 <yashmp2004@gmail.com>
2026-01-29 11:30:08 +0530

    dom task-01

---
+++ lecture-05/DOM/my-practice/task-01.js
@@ -0,0 +1,9 @@
+/* Problem : Write a function changeText that selects an HTML element by its ID and updates its text content to a specific value. */
+
+function changeText (elementId, newText) {
+    let element = document.getElementById(elementId);
+    element.textContent = newText;
+}
+changeText("message", "New");
+changeText("header", "Welcome");
+changeText("status", "Online");
